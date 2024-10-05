e9d614877fbcb1d6ea68acd55dcd6ae1b627d421
trgamedou <158215317+trgamedou@users.noreply.github.com>
2024-10-05 15:10:57 +0000

    updated

--- 1.py
+++ 1.py
@@ -5,59 +5,71 @@ from shapely.geometry import box
 import pandas as pd
 import logging
 from argparse import ArgumentParser
+from datetime import datetime
 
-def main(year):
-    
-    logging.basicConfig(level=logging.INFO)
+print(f'0 : {datetime.now()}')
 
-    world = gpd.read_file("./data/ne_110m_admin_0_countries.shp")
+year = 2020
 
-    # Drop uninhabited regions and Antarctica
-    world = world[(world.POP_EST > 0) & (world.NAME != "Antarctica")]
+logging.basicConfig(level=logging.INFO)
 
-    region = world
-    region_name = "world"
+world = gpd.read_file("./data/ne_110m_admin_0_countries.shp")
 
-    # Loop over the years
-    logging.info(f"Processing {year}")
+# Drop uninhabited regions and Antarctica
+world = world[(world.POP_EST > 0) & (world.NAME != "Antarctica")]
 
-    # Define the cutout; this will not yet trigger any major operations
-    cutout = atlite.Cutout(
-        path=f"{region_name}-{year}_timeseries", module="era5", 
-        bounds=region.unary_union.bounds, 
-        time=f"{year}",
-        chunks={"time": 100,},)
-    # This is where all the work happens (this can take some time).
-    cutout.prepare(
-        compression={"zlib": True, "complevel": 9},
-        monthly_requests=True,
-        concurrent_requests=True)
+region = world
+region_name = "western-europe"
 
-    # Extract the wind power generation capacity factors
-    wind_power_generation = cutout.wind(
-        "Vestas_V112_3MW", 
-        capacity_factor_timeseries=True,
-        )
+# Loop over the years
+logging.info(f"Processing {year}")
 
-    # Extract the solar power generation capacity factors
-    solar_power_generation = cutout.pv(
-        panel="CSi", 
-        orientation='latitude_optimal', 
-        tracking="horizontal",
-        capacity_factor_timeseries=True,)
-    
-    # Extract the concenctrated solar power generation capacity factors
-    csp_power_generation = cutout.csp(
-        installation="SAM_parabolic_trough", 
-        capacity_factor_timeseries=True,)
+print(f'1 : {datetime.now()}')
 
-    # Save gridded data as netCDF files
-    wind_power_generation.to_netcdf(f"{region_name}_wind_CF_timeseries_{year}.nc")
-    solar_power_generation.to_netcdf(f"{region_name}_solar_CF_timeseries_{year}.nc")
-    csp_power_generation.to_netcdf(f"{region_name}_csp_CF_timeseries_{year}.nc")
+# Define the cutout; this will not yet trigger any major operations
+cutout = atlite.Cutout(
+    path=f"{region_name}-2011-01.nc",
+    module="era5",
+    x=slice(-13.6913, 1.7712),
+    y=slice(49.9096, 60.8479),
+    time="2011-01",
+)
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--year", type=int, help="Get data for this year")
-    args = parser.parse_args()
-    main(args.year)
+# This is where all the work happens (this can take some time).
+cutout.prepare(
+    compression={"zlib": True, "complevel": 9},
+    monthly_requests=True,
+    concurrent_requests=True)
+
+print(f'2 : {datetime.now()}')
+
+# Extract the wind power generation capacity factors
+wind_power_generation = cutout.wind(
+    "Vestas_V112_3MW", 
+    capacity_factor_timeseries=True,
+    )
+
+print(f'3 : {datetime.now()}')
+
+# Extract the solar power generation capacity factors
+solar_power_generation = cutout.pv(
+    panel="CSi", 
+    orientation='latitude_optimal', 
+    tracking="horizontal",
+    capacity_factor_timeseries=True,)
+
+print(f'4 : {datetime.now()}')
+
+# Extract the concenctrated solar power generation capacity factors
+csp_power_generation = cutout.csp(
+    installation="SAM_parabolic_trough", 
+    capacity_factor_timeseries=True,)
+
+print(f'5 : {datetime.now()}')
+
+# Save gridded data as netCDF files
+wind_power_generation.to_netcdf(f"{region_name}_wind_CF_timeseries_{year}.nc")
+solar_power_generation.to_netcdf(f"{region_name}_solar_CF_timeseries_{year}.nc")
+csp_power_generation.to_netcdf(f"{region_name}_csp_CF_timeseries_{year}.nc")
+
+print(f'6 : {datetime.now()}')
