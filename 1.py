80f0a95f1418762fac2d970f4603b8530e391ee0
trgamedou <158215317+trgamedou@users.noreply.github.com>
2024-10-05 20:38:51 +0000

    update

--- 1.py
+++ 1.py
@@ -16,32 +16,25 @@ logging.basicConfig(level=logging.INFO)
 world = gpd.read_file("./data/ne_110m_admin_0_countries.shp")
 
 # Drop uninhabited regions and Antarctica
-world = world[(world.POP_EST > 0) & (world.NAME != "Antarctica")]
+world = world[(world.pop_est > 0) & (world.name != "Antarctica")]
 
 region = world
-region_name = "western-europe"
+region_name = "world"
 
 # Loop over the years
 logging.info(f"Processing {year}")
 
-print(f'1 : {datetime.now()}')
-
 # Define the cutout; this will not yet trigger any major operations
 cutout = atlite.Cutout(
-    path=f"{region_name}-2011-01.nc",
-    module="era5",
-    x=slice(-13.6913, 1.7712),
-    y=slice(49.9096, 60.8479),
-    time="2011-01",
-)
-
+    path=f"{region_name}-{year}_timeseries", module="era5", 
+    bounds=region.unary_union.bounds, 
+    time=f"{year}",
+    chunks={"time": 100,},)
 # This is where all the work happens (this can take some time).
 cutout.prepare(
     compression={"zlib": True, "complevel": 9},
     monthly_requests=True,
     concurrent_requests=True)
-
-print(f'2 : {datetime.now()}')
 
 # Extract the wind power generation capacity factors
 wind_power_generation = cutout.wind(
